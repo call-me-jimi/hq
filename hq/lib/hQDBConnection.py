d302c2b9f5e6d13670d9cf48444ab44ea5c05a5d
Hendrik <hendrik@funkverband.de>
2015-08-27 23:00:28 +0200

    bugfix

--- hq/lib/hQDBConnection.py
+++ hq/lib/hQDBConnection.py
@@ -4,7 +4,7 @@ import sys
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-import hq.lib.hQDBSessionRegistry
+import hq.lib.hQDBSessionRegistry as hQDBSessionRegistry
 
 DBSession = hQDBSessionRegistry.DBSession
 engine = hQDBSessionRegistry.engine
